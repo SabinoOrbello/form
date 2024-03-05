41d045fa6d675f5856ea6247a0e9359f195163ed
SabinoOrbello <gemellus95@outlook.it>
2024-03-05 09:09:24 +0100

    Aggiungere i file di progetto.

---
+++ form/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace form.Models
+{
+    public class Login
+    {
+        [checkEmail(ListaEmailString = "ebbystronzo@gmail.com,macschifo@gmail.com,sabinochefai@gmail.com", ErrorMessage = ("l'Email e' sbagliata"))]
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ form/checkEmail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace form
+{
+    public class checkEmail : ValidationAttribute
+    {
+        public string ListaEmailString { get; set; }
+        public checkEmail() { }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string[] listaEmail = ListaEmailString.ToString().Split(',');
+            if (listaEmail.Contains(value))
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("Non e' ammesso");
+            }
+        }
+    }
+}
